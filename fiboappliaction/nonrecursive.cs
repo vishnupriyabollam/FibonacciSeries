cc963c9d08c6e22f542071ca2963a566ee4db000
vishnupriyabollam <vishnupriya.bollam@neudesic.com>
2018-06-08 00:03:34 +0530

    fibonacci series

---
+++ fiboappliaction/nonrecursive.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fiboappliaction
+{
+    class nonrecursive
+    {
+        static void Main(string[] args)
+        {
+            long fiboinput,first=0,second=1,third=0;
+            Console.WriteLine("enter the number");
+            fiboinput = long.Parse(Console.ReadLine());
+            Console.WriteLine("0 1");
+            for(int fibo=2;fibo<=fiboinput;fibo++)
+            {
+                third = first + second;
+                first = second;
+                second = third;
+                Console.WriteLine(third);
+            }
+            Console.ReadKey();  
+        }
+    }
+}
---
+++ fiboappliaction/recursivefibo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fiboappliaction
+{
+    public interface recursivefibo
+    {
+       public void fibo();
+        {
+            int input;
+            Console.WriteLine("read");
+            input = int.Parse(Console.ReadLine());
+            Console.WriteLine("read");
+            Console.ReadKey();
+        }
+            
+    }
+
+
+    
+}
